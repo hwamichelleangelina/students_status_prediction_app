9d9b77f1a60dd2f648c9b544d17ba31bf538876c
Michelle Angelina <160707395+hwamichelleangelina@users.noreply.github.com>
2025-05-02 13:44:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ with st.form("student_form"):
 
         unemployment_rate = st.number_input("Unemployment rate: ", min_value=0.0, step=0.1)
         inflation_rate = st.number_input("Inflation rate: ", min_value=0.0, step=0.1)
-        gdp = st.number_input("GDP: ", min_value=0.0, step=0.1)
+        gdp = st.number_input("GDP: ", min_value=-100.0, step=0.1)
 
     submitted = st.form_submit_button("Prediksi Status Mahasiswa")
 
